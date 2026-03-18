958b0fddf8302cf44466630fa4b712265316df2d
Abhinav <abhisinghbsn08@gmail.com>
2026-03-18 19:17:31 +0530

    abhinav/phase2-historical-var

--- config/config_loader.py
+++ config/config_loader.py
@@ -9,3 +9,20 @@ DEFAULT_CONFIG = {
     "time_horizon" : 1,
     "num_simulations" : 10000
 }
+def load_config(path : str = None)-> dict :
+    """
+    Loads configuration parameters
+    If json file is provided then it is loaded from that
+    else returns default config
+    Returns dict of config
+    """
+    if path is None:
+        return DEFAULT_CONFIG.copy()
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"Config file not found at {path}")
+    with open (path , "r") as f:
+        config = json.load(f)
+    # means if there are any missing keys then use the default values
+    for key,value in DEFAULT_CONFIG.items():
+        config.setdefault(key,value)
+    return config
---
+++ risk/historical_var.py
@@ -0,0 +1,36 @@
+import pandas as pd 
+import numpy as np
+
+def compute_historical_var(returns: pd.series, confidence_level: float = 0.95) ->float:
+
+    """ 
+    Computes the historical VaR
+    returns : series of log returns
+    confidence_level = 0.95
+    returns VaR as a float"""
+    if returns.empty:
+        raise ValueError("Returns series is empty")
+    if not (0<confidence_level<1):
+        raise ValueError("Confidence level must be within 0 and 1")
+    # taking the 5th percentile of the returns(the worst 5% of days)
+    # doing - as VaR is positive and var is supposed to be positive
+
+    var = -np.percentile(returns,(1-confidence_level)*100)
+    return float(var)
+
+def compute_historical_cvar(returns:pd.series, confidence_level:float=0.95)->float:
+    """ 
+    Computes the historical CVaR
+    returns : series of log returns
+    confidence_level = 0.95
+    returns CVaR as a float"""
+    if returns.empty:
+        raise ValueError("Returns series is empty") 
+    if not (0<confidence_level<1):
+        raise ValueError("Confidence level must be within 0 and 1")
+    
+    losses = -returns
+    var = compute_historical_var(returns,confidence_level)
+    #cvar is average of the worst 5% of the days
+    cvar = losses[losses>=var].mean()
+    return float(cvar)
